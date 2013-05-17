6b3e2c3ff8bb605312465b75f17307d59db85e42
Dmitry Papchenkov <hidden>
2013-05-17 18:58:52 +0400

    fixed naming of orig tarball

--- create-orig-tarball.sh
+++ create-orig-tarball.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 VERSION=$(egrep '^VERSION' config.mk)
 VERSION=`echo ${VERSION#*=}` # ugly way to strip spaces
-TARFILE=../utmp-$VERSION.orig.tar.bz2
+TARFILE=../utmp-${VERSION}_orig.tar.bz2
 tar -c -j -f$TARFILE --exclude-vcs --exclude=debian/* .
 _rc=$?
 if [ $_rc -eq 0 ]; then
